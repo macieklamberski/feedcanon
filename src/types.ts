924d9c7559252415af3f004f74a6e621711633eb
Maciej Lamberski <maciej@lamberski.com>
2025-12-13 18:24:00 +0100

    feat: Add basic type definitions

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+// Options for fetch function.
+export type FetchFnOptions = {
+  method?: 'GET' | 'HEAD'
+  headers?: Record<string, string>
+}
+
+// Response from fetch function (normalized across adapters).
+export type FetchFnResponse = {
+  headers: Headers
+  body: string
+  url: string // Final URL after redirects.
+  status: number
+}
+
+// Custom fetch function type (adapter interface).
+export type FetchFn = (url: string, options?: FetchFnOptions) => Promise<FetchFnResponse>
+
+// Verification function type.
+export type VerifyFn = (url: string) => boolean | Promise<boolean>
+
+// Hash function type.
+export type HashFn = (content: string) => string | Promise<string>
