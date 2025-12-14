057863dc95f4d9b731e31ecf3a187061f96d1f10
Maciej Lamberski <maciej@lamberski.com>
2025-12-14 18:08:00 +0100

    feat: Add defaultFetchFn using native fetch

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,4 +1,4 @@
-import type { NormalizeOptions } from './types.js'
+import type { FetchFn, NormalizeOptions } from './types.js'
 
 // Known feed-related protocol schemes that should be converted to https://.
 export const defaultFeedProtocols = ['feed:', 'rss:', 'pcast:', 'itpc:']
@@ -7,4 +7,18 @@ export const defaultNormalizeOptions: NormalizeOptions = {
   protocol: true,
   www: true,
   trailingSlash: true,
+}
+
+export const defaultFetchFn: FetchFn = async (url, options) => {
+  const response = await fetch(url, {
+    method: options?.method || 'GET',
+    headers: options?.headers,
+  })
+
+  return {
+    headers: response.headers,
+    body: await response.text(),
+    url: response.url,
+    status: response.status,
+  }
 }
