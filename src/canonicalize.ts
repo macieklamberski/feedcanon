555b1dbaf12a2a5dfd7d58e6e112adb4daa69749
Maciej Lamberski <maciej@lamberski.com>
2025-12-21 16:52:45 +0100

    style: Remove comment

--- src/canonicalize.ts
+++ src/canonicalize.ts
@@ -104,9 +104,7 @@ export const canonicalize = async <T>(
 
           return initialResponseSignature === comparedResponseSignature
         }
-      } catch {
-        // Parsing failed, signatures don't match.
-      }
+      } catch {}
     }
 
     return false
